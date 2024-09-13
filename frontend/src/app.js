7337540c3c39051859a150c30240e381f5acc760
premnath84 <premnath.web84@gmail.com>
2024-09-13 23:34:35 +0530

    added signin page (blank)

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,11 +3,13 @@ import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
 import { parseRequestUrl } from './utils.js';
 import Error404Screen from './screens/Error404Screen.js';
+import SigninScreen from './screens/SignInScreen.js';
 
 // create routes object with url as key and screen as value.
 const routes ={
     '/': HomeScreen,
     '/product/:id': ProductScreen,
+    '/signin' : SigninScreen,
 };
 
 
